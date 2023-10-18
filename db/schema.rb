9e9edd629df0a20a5b9846283e933cbe58993526
Orcun Tonyali <tonyaliorcun@gmail.com>
2023-10-18 16:20:44 +0200

    seed-added

--- db/schema.rb
+++ db/schema.rb
@@ -59,6 +59,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_16_141207) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name", default: "", null: false
+    t.string "surname", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
