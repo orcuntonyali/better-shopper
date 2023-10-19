86b8b5d2cab4a0c5b2f0928e8bc5bd2b25fef21e
Shamaila Sadaf <i>
2023-10-19 15:19:07 +0200

    map added to homepage for user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,17 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @users = User.all
+    @markers = @users.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude,
+        info_window_html: render_to_string(partial: "popup", locals: { user: user})
+      }
+    end
   end
+  end
+
+  def index
+
 end
