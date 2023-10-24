fb32bfe11e0cadaf081b702bee07f047ec40bcaa
Shamaila Sadaf <i>
2023-10-24 17:48:05 +0200

    aded search function in map on clicking change address button

--- app/javascript/controllers/buttons_controller.js
+++ app/javascript/controllers/buttons_controller.js
@@ -1,14 +1,32 @@
-import { Controller } from "@hotwired/stimulus"
-
+import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
-  static targets = ["address"]
-
-  changeAddress() {
-    const userAddress = this.data.get("userAddress")
-    this.addressTarget.textContent = userAddress
+  static targets = ["address"];
+  static values = {
+    mapController: String
   }
-
+  connect() {
+    this.element.addEventListener("click", (event) => {
+      event.preventDefault();
+       });
+  }
+  changeAddress() {
+    // const searchbox = document.querySelector(".search-box");
+    // searchbox.style.display = "block";
+    const changeAddressButton = this.element.querySelector(".change-address-button");
+    changeAddressButton.style.display = "none";
+    const userAddress = document.querySelector(".user-details");
+    userAddress.style.display = "none";
+    const mapController = this.application.getControllerForElementAndIdentifier(
+      document.getElementById('map'),
+      "map"
+    );
+    if (mapController) {
+      mapController.showMapboxSearchBox();
+    }
+  }
   lookingGood() {
-    // Add behavior for the "LOOKING GOOD" button here if needed.
+    if (confirm("Proceed to cart_items/new?")) {
+      window.location.href = "/cart_items/new";
+    }
   }
 }
