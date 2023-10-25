6f51313b014273ab054b2341aa8c029ee1d56478
Orcun Tonyali <tonyaliorcun@gmail.com>
2023-10-25 12:04:03 +0200

    loco

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,6 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :cart_items
 
-  validates :status, presence: true, inclusion: { in: [some_array_of_valid_statuses] }
+  validates :status, presence: true, inclusion: { in: [:pending, :shipped, :cancelled] }
   validates :delivery_option, inclusion: { in: [true, false] }
 end
