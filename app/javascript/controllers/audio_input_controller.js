de5e4e4e7639b216d3b4f88557c4823f83ef0be0
Orcun Tonyali <tonyaliorcun@gmail.com>
2023-11-06 15:18:12 +0100

    cart-items-done-whitespace-problem-wip

--- app/javascript/controllers/audio_input_controller.js
+++ app/javascript/controllers/audio_input_controller.js
@@ -1,15 +1,39 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["transcribedText", "recordButton", "textAreaFrame", "submitButton"];
+  static targets = ["transcribedText", "recordButton", "submitButton"];
 
   connect() {
     console.log("Stimulus controller connected");
     this.isRecording = false;
     this.mediaRecorder = null;
     this.audioChunks = [];
-    this.loadTextAreaContent();
 
+    // Retrieve and set the textarea content from localStorage if available and not just whitespace
+    const savedText = localStorage.getItem('transcribedText');
+    if (savedText && savedText.trim().length > 0 && this.hasTranscribedTextTarget) {
+      this.transcribedTextTarget.value = savedText.trim();
+      this.submitButtonTarget.classList.remove('hidden');
+    } else {
+      localStorage.removeItem('transcribedText'); // Clear whitespace or empty strings
+      this.submitButtonTarget.classList.add('hidden');
+    }
+
+    // Listen to changes in the textarea and update localStorage and button visibility
+    if (this.hasTranscribedTextTarget) {
+      this.transcribedTextTarget.addEventListener('input', () => {
+        const textValue = this.transcribedTextTarget.value;
+        if (textValue.trim().length > 0) {
+          localStorage.setItem('transcribedText', textValue);
+          this.submitButtonTarget.classList.remove('hidden');
+        } else {
+          localStorage.removeItem('transcribedText');
+          this.submitButtonTarget.classList.add('hidden');
+        }
+      });
+    }
+
+    // Initialize media recorder
     navigator.mediaDevices.getUserMedia({ audio: true })
       .then(stream => {
         this.mediaRecorder = new MediaRecorder(stream);
@@ -19,19 +43,7 @@ export default class extends Controller {
       .catch(err => console.error("Error initializing media recorder:", err));
   }
 
-  loadTextAreaContent() {
-    const savedText = localStorage.getItem('transcribedText');
-    if (savedText) {
-      this.showTextArea(savedText);
-    }
-  }
-
   toggleRecording() {
-    if (!this.isInitialized) {
-      this.initializeTextArea();
-      return; // Early return to avoid starting recording immediately
-    }
-
     if (!this.isRecording) {
       this.startRecording();
     } else {
@@ -40,36 +52,24 @@ export default class extends Controller {
   }
 
   startRecording() {
-    if (this.mediaRecorder.state === "inactive") {
-      console.log("Start recording triggered");
-      this.audioChunks = [];
+    if (this.mediaRecorder && this.mediaRecorder.state === "inactive") {
       this.mediaRecorder.start();
-      this.isRecording = true; // Set the recording state to true
-
-      const recordText = this.recordButtonTarget.querySelector('.record-text');
-      if (recordText) {
-        recordText.textContent = "Recording...";
-        this.recordButtonTarget.classList.remove("violet");
-        this.recordButtonTarget.classList.add("gray");
-      } else {
-        console.error('The record-text element does not exist!');
-      }
+      this.isRecording = true; // Update recording state
+      // Update UI for recording state
+      this.recordButtonTarget.classList.add("gray");
+      this.recordButtonTarget.querySelector('.record-icon').classList.add('active');
+      this.recordButtonTarget.querySelector('.record-text').textContent = "Recording...";
     }
   }
 
   stopRecording() {
     if (this.mediaRecorder && this.mediaRecorder.state === "recording") {
       this.mediaRecorder.stop();
-      this.isRecording = false; // Set the recording state to false
-
-      const recordText = this.recordButtonTarget.querySelector('.record-text');
-      if (recordText) {
-        recordText.textContent = "Tap to Speak";
-        this.recordButtonTarget.classList.remove("gray");
-        this.recordButtonTarget.classList.add("violet");
-      } else {
-        console.error('The record-text element does not exist!');
-      }
+      this.isRecording = false; // Update recording state
+      // Update UI for not recording state
+      this.recordButtonTarget.classList.remove("gray");
+      this.recordButtonTarget.querySelector('.record-icon').classList.remove('active');
+      this.recordButtonTarget.querySelector('.record-text').textContent = "Tap to Speak";
     }
   }
 
@@ -104,69 +104,11 @@ export default class extends Controller {
     });
   }
 
-  initializeTextArea() {
-    this.hideQuickTips();
-    this.showTextArea();
-
-    // Hide 'TAP TO START' button
-    const tapToStartButton = this.element.querySelector('[data-action="click->audio-input#initializeTextArea"]');
-    tapToStartButton.classList.add('hidden');
-
-    // Reveal and initialize the record button for recording actions
-    this.recordButtonTarget.classList.remove('hidden');
-
-    const recordText = this.recordButtonTarget.querySelector('.record-text');
-    if (recordText) {
-      recordText.textContent = "Tap to Speak";
-    } else {
-      console.error('The record-text element does not exist!');
-    }
-    this.isInitialized = true;
-  }
-
-  showTextArea(newText = '') {
-    const quickTips = "Quick Tips:\n1. Speak or Type\n2. Edit\n3. Press 'Find groceries'";
-
-    // Check if newText is an event object and reset it if so
-    if (typeof newText === 'object') {
-      newText = '';
-    }
-
-    let textarea = this.textAreaFrameTarget.querySelector('.transcribed-text');
-
-    if (!textarea) {
-      // If textarea doesn't exist, create it and set the placeholder
-      textarea = document.createElement('textarea');
-      textarea.className = 'transcribed-text h--100';
-      textarea.id = 'transcribedText';
-      textarea.setAttribute('data-audio-input-target', 'transcribedText');
-      textarea.placeholder = quickTips; // Set the placeholder text
-      this.textAreaFrameTarget.appendChild(textarea);
-    }
-
-    // If there's newText, add it to the textarea and show the submit button
-    if (newText) {
-      textarea.value = newText; // Set new text if provided
+  showTextArea(transcribedText) {
+    if (this.hasTranscribedTextTarget) {
+      this.transcribedTextTarget.value = transcribedText;
       this.submitButtonTarget.classList.remove('hidden');
-    } else {
-      // If there's saved text in localStorage, use it, otherwise show placeholder
-      const savedText = localStorage.getItem('transcribedText');
-      textarea.value = savedText ? savedText : '';
-      this.submitButtonTarget.classList.toggle('hidden', !savedText);
-    }
-
-    // Update localStorage when textarea content changes
-    textarea.addEventListener('input', () => {
-      const textValue = textarea.value;
-      this.submitButtonTarget.classList.toggle('hidden', !textValue.trim()); // Hide submit button if textarea is empty
-      localStorage.setItem('transcribedText', textValue);
-    });
-  }
-
-  hideQuickTips() {
-    const quickTips = this.element.querySelector('.quick-tips');
-    if (quickTips) {
-      quickTips.classList.add('hidden');
+      localStorage.setItem('transcribedText', transcribedText);
     }
   }
 
@@ -193,5 +135,4 @@ export default class extends Controller {
       console.log("Order processing failed:", error);
     });
   }
-
 }
